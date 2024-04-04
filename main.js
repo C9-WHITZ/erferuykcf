29134b6e719ac27770c9ceac2e17998dd07550e2
C9-WHITZ <142932816+C9-WHITZ@users.noreply.github.com>
2024-04-04 15:53:21 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+ var   Imagens = ["1.jpg","2.avif","3.jpeg","4.avif"]
+
+ var frases = ["As árvores fornecem oxigênio",
+  "As árvores fornecem matéria prima",
+   "As árvores fornecem frutos",
+    "As árvores fornecem sombras"];
+
+var i = 0;
+
+    function proximo(){
+        i++;
+document.getElementById("fotos").scr=imagens[i];
+document.getElementById("mensagem").innerHTML=frase
+    }
